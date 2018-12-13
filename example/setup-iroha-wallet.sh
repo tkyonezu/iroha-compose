6b7d8200f3066f199f132dd2d2be128ff8082168
Sleepy Senzu <tkyonezu@gmail.com>
2018-12-14 00:32:39 +0900

    Need more time until the first CreateAccount completion, and so on...

--- example/setup-iroha-wallet.sh
+++ example/setup-iroha-wallet.sh
@@ -8,6 +8,8 @@ CONTAINER_NAME="iroha-node1"
 IROHA_IP=0.0.0.0
 IROHA_PORT=50051
 
+WAIT_TIME=5
+
 function tx {
   echo -e "\n>>> $1 ($3 $4 $5 $6 $7 $8) <<<" | sed 's/ *)/)/'
 
@@ -51,43 +53,44 @@ EOF
 }
 
 tx CreateAccount crt_acc alice test $(cat alice@test.pub)
-sleep 1
+sleep ${WAIT_TIME}
+sleep ${WAIT_TIME}
 rx GetAccountInformation get_acc alice@test
 
 tx CreateAccount crt_acc bob test $(cat bob@test.pub)
-sleep 1
+sleep ${WAIT_TIME}
 rx GetAccountInformation get_acc bob@test
 
 tx CreateAsset crt_ast coolcoin test 2
-sleep 1
+sleep ${WAIT_TIME}
 rx GetAssetInformation get_ast_info 'coolcoin#test'
 
 tx CreateAsset crt_ast hotcoin test 5
-sleep 1
+sleep ${WAIT_TIME}
 rx GetAssetInformation get_ast_info 'hotcoin#test'
 
 tx AddAssetQuantity add_ast_qty 'coolcoin#test' 1000 0
-sleep 1
+sleep ${WAIT_TIME}
 rx GetAccountAsset get_acc_ast admin@test 'coolcoin#test'
 
 tx AddAssetQuantity add_ast_qty 'hotcoin#test' 1000 0
-sleep 1
+sleep ${WAIT_TIME}
 rx GetAccountAsset get_acc_ast admin@test 'hotcoin#test'
 
 tx TransferAsset tran_ast admin@test alice@test 'coolcoin#test' 500.00
-sleep 1
+sleep ${WAIT_TIME}
 rx GetAccountAsset get_acc_ast alice@test 'coolcoin#test'
 
 tx TransferAsset tran_ast admin@test alice@test 'hotcoin#test' 500.00000
-sleep 1
+sleep ${WAIT_TIME}
 rx GetAccountAsset get_acc_ast alice@test 'hotcoin#test'
 
 tx TransferAsset tran_ast admin@test bob@test 'coolcoin#test' 500.00
-sleep 1
+sleep ${WAIT_TIME}
 rx GetAccountAsset get_acc_ast bob@test 'coolcoin#test'
 
 tx TransferAsset tran_ast admin@test bob@test 'hotcoin#test' 500.00000
-sleep 1
+sleep ${WAIT_TIME}
 rx GetAccountAsset get_acc_ast bob@test 'hotcoin#test'
 
 exit 0
