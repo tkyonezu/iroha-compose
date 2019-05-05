adff4bb9bcae2fa5a6ca59fb9e1ce9099d2236f4
Sleepy Senzu <tkyonezu@gmail.com>
2019-05-05 14:52:07 +0900

    Change comments

--- scripts/iroha-newkey.sh
+++ scripts/iroha-newkey.sh
@@ -7,7 +7,7 @@
 #
 
 if [ $# -lt 1 ]; then
-  echo "Usage: iroha-newaccount <account_name|node_name>"
+  echo "Usage: iroha-newaccount {<account_name>|<node_name>}"
   exit 1
 fi
 
@@ -16,6 +16,5 @@ docker exec -i iroha_node_1 iroha-cli --new_account --account_name $1
 if [ -d example/multi-node ]; then
   mv example/$1.* example/multi-node
 fi
-
 
 exit 0
