adc4740deb49518f6116a4d0cc1213194065b6bc
Sleepy Senzu <tkyonezu@gmail.com>
2019-05-06 22:53:45 +0900

    Add script for setting multi-node ip addresses in genesis_block.

---
+++ scripts/iroha-peers.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) 2019 Takeshi Yonezu
+# All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+GENESIS_BLOCK=example/multi-node/genesis.block
+
+if [ $# -lt 4 ]; then
+  echo "Usage: iroha-peers: <address1> <address2> <address3> <address4>"
+  exit 1
+fi
+
+cat ${GENESIS_BLOCK}.in |
+  sed -e "s/IP1/$1/" -e "s/IP2/$2/" -e "s/IP3/$3/" -e "s/IP4/$4/" >${GENESIS_BLOCK}
+
+grep address ${GENESIS_BLOCK}
+
+exit 0
