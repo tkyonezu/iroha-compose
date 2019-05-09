9e546e44e8d20f6266c477d10e5ef48178e79364
Sleepy Senzu <tkyonezu@gmail.com>
2019-05-09 09:59:32 +0900

    Add more functions.

--- scripts/iroha-addpeer.sh
+++ scripts/iroha-addpeer.sh
@@ -6,31 +6,57 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-if [ $# -eq 0 ]; then
+function usage {
   echo "Usage: iroha-addpeer <node_name>"
-  echo "       iroha-addpeer [ down | restart ]"
+  echo "       iroha-addpeer [ restart | logs | down | clean ]"
   exit 0
-fi
+}
 
-if [[ $# -eq 1 && ("$1" = "down" || "$1" = "restart")]]; then
-  cd example/multi-node
-
-  echo "$ docker-compose -f docker-compose.yml $1"
-  docker-compose -f docker-compose.yml $1
-
-  exit 0
+if [ $# -eq 0 ]; then
+  usage
 fi
 
 cd example/multi-node
 
-if [ $# -ge 1 ]; then
-  IROHA_NODEKEY=$1
-else
-  IROHA_NODEKEY=node0
-fi
+case "$1" in
+  logs)
+    echo "$ docker logs -f iroha_node_1 &"
+    docker logs -f iroha_node_1 &
+    exit 0;;
+  restart)
+    echo "$ docker-compose -f docker-compose.yml $1"
+    docker-compose -f docker-compose.yml $1
+
+    exit 0;;
+  down)
+    echo "$ docker-compose -f docker-compose.yml $1"
+    docker-compose -f docker-compose.yml $1
+
+    echo "$ docker volume prune -f"
+    docker volume prune -f
+
+    exit 0;;
+  clean)
+    echo "$ rm -f block_store/0*"
+    rm -f block_store/0*
+    exit 0;;
+esac
+
+echo "Cleanup current block_store data:"
+echo "$ rm -f block_store/0*"
+rm -f block_store/0*
+
+echo "Current node:"
+echo "    $(grep IROHA_NODEKEY docker-compose.yml | sed 's/.*IROHA_NODEKEY=//')"
+
+PEERS=$(grep address genesis.block | sed 's/.*address":"/    /' | sed 's/",//')
+echo "Peers:"
+echo "${PEERS}"
+
+exit 0
 
 cat docker-compose.yml.in |
-  sed -e "s/IROHA_NODEKEY=.*/IROHA_NODEKEY=${IROHA_NODEKEY}/" >docker-compose.yml
+  sed -e "s/IROHA_NODEKEY=.*/IROHA_NODEKEY=${1}/" >docker-compose.yml
 
 echo "$ docker-compose -f docker-compose.yml up -d"
 docker-compose -f docker-compose.yml up -d
